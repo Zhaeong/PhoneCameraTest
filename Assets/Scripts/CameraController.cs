7a67f065b91fce8cee30afee149690cb30bc53bd
Zhaeong <owen_zzz@hotmail.com>
2017-04-12 00:28:12 -0400

    first commit, added camera plane

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public WebCamTexture mCamera = null;
+    public GameObject plane;
+
+    // Use this for initialization
+    void Start () {
+
+
+        mCamera = new WebCamTexture();
+        plane.GetComponent<Renderer>().material.mainTexture = mCamera;
+
+        mCamera.Play();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
